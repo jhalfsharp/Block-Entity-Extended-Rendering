c5809ff55f4b0c7c94cb63b5dee7b39bcf97f8c1
jimbot6000 <55899954+jimbot6000@users.noreply.github.com>
2022-06-04 20:39:55 -0700

    Fix mixin
    Made mixin function, allowing mod to actually work
    Adjusted metadata (.jar name, version, homepage)

---
+++ remappedSrc/net/jimbot6000/blockentityextendedrendering/gui/BEERConfig.java
@@ -0,0 +1,67 @@
+package net.jimbot6000.blockentityextendedrendering.gui;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.jimbot6000.blockentityextendedrendering.BlockEntityExtendedRendering;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class BEERConfig {
+    public static final String BLOCK_ENTITY_RENDER_DISTANCE_KEY = "block_entity_render_distance";
+
+    public int BLOCK_ENTITY_RENDER_DISTANCE = 128;
+
+    public void writeTo(Properties properties) {
+        properties.setProperty(BLOCK_ENTITY_RENDER_DISTANCE_KEY, Integer.toString(BLOCK_ENTITY_RENDER_DISTANCE));
+    }
+
+    public void readFrom(Properties properties) {
+        this.BLOCK_ENTITY_RENDER_DISTANCE = Integer.parseInt(properties.getProperty(BLOCK_ENTITY_RENDER_DISTANCE_KEY));
+    }
+
+    public void save() {
+        Properties properties = new Properties();
+        writeTo(properties);
+        Path configPath = FabricLoader.getInstance().getConfigDir().resolve("block_entity_extended_rendering.properties");
+        if(!Files.exists(configPath)) {
+            try {
+                Files.createFile(configPath);
+            } catch (IOException e) {
+                BlockEntityExtendedRendering.LOGGER.error("Failed to create configuration file!");
+                e.printStackTrace();
+                return;
+            }
+        }
+        try {
+            properties.store(Files.newOutputStream(configPath), "Configuration file for BEER");
+        } catch (IOException e) {
+            BlockEntityExtendedRendering.LOGGER.error("Failed to write to configuration file!");
+            e.printStackTrace();
+        }
+    }
+
+    public void load() {
+        Properties properties = new Properties();
+        Path configPath = FabricLoader.getInstance().getConfigDir().resolve("block_entity_extended_rendering.properties");
+        if(!Files.exists(configPath)) {
+            try {
+                Files.createFile(configPath);
+                save();
+            } catch (IOException e) {
+                BlockEntityExtendedRendering.LOGGER.error("Failed to create configuration file!");
+                e.printStackTrace();
+                return;
+            }
+        }
+        try {
+            properties.load(Files.newInputStream(configPath));
+        } catch (IOException e) {
+            BlockEntityExtendedRendering.LOGGER.error("Failed to read configuration file!");
+            e.printStackTrace();
+            return;
+        }
+        readFrom(properties);
+    }
+}
--- remappedSrc/net/jimbot6000/blockentityextendedrendering/mixin/RenderDistanceIncrease.java
+++ remappedSrc/net/jimbot6000/blockentityextendedrendering/mixin/RenderDistanceIncrease.java
@@ -5,15 +5,15 @@ import net.jimbot6000.blockentityextendedrendering.gui.BEERConfig;
 import net.jimbot6000.blockentityextendedrendering.utils.GetDistanceInterface;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
 import java.util.Properties;
 
 @Mixin(BlockEntityRenderer.class)
 public interface RenderDistanceIncrease extends GetDistanceInterface {
 
-	@Overwrite
+	@Override
 	default int getRenderDistance() {
+		BlockEntityExtendedRendering.LOGGER.info("mixin used");
 		Properties config = new Properties();
 		BlockEntityExtendedRendering.CONFIG.writeTo(config);
 		return Integer.parseInt(config.getProperty(BEERConfig.BLOCK_ENTITY_RENDER_DISTANCE_KEY));
